0bd6b115ed6ec34667e43a6e6c3b78ceeb5125a1
Nayab-Gauhar <nayabgauhar8@gmail.com>
2026-08-20 00:50:00 +0530

    Making my own mouse contrilling using eye

--- src/controlling_mouse/main.py
+++ src/controlling_mouse/main.py
@@ -1,2 +1,15 @@
-import cv2
-import numpy as np
+import pyautogui
+import time
+
+print("Current pos:", pyautogui.position())
+
+pyautogui.moveTo(100, 100, duration=1)
+print("Moved to top-left area:", pyautogui.position())
+print("Current pos:", pyautogui.position())
+
+
+time.sleep(1)
+
+pyautogui.moveTo(1500, 800, duration=1)
+print("Moved to bottom-right area:", pyautogui.position())
+print("Current pos:", pyautogui.position())
