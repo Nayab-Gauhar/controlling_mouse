ab65406b328a22d010b3657973228e5bf13b4188
Nayab-Gauhar <nayabgauhar8@gmail.com>
2026-08-20 12:18:18 +0530

    Add dlib dependency and implement face detection in main.py

--- src/controlling_mouse/main.py
+++ src/controlling_mouse/main.py
@@ -1,15 +1,23 @@
 import pyautogui
 import time
+import cv2
+import dlib
 
-print("Current pos:", pyautogui.position())
-
-pyautogui.moveTo(100, 100, duration=1)
-print("Moved to top-left area:", pyautogui.position())
-print("Current pos:", pyautogui.position())
+vid = cv2.VideoCapture(0)
+find_faces = dlib.get_frontal_face_detector()
+find_landmark = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 
 
-time.sleep(1)
-
-pyautogui.moveTo(1500, 800, duration=1)
-print("Moved to bottom-right area:", pyautogui.position())
-print("Current pos:", pyautogui.position())
+while True:
+    ret,frame = vid.read()
+    for faces in find_faces(frame):
+        x1,y1 = faces.left(),faces.top()
+        x2,y2 = faces.right(),faces.bottom()
+        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),3)
+        landmarks = find_landmark(frame,faces).parts()
+        for landmark in landmarks:
+            cv2.circle(frame,(landmark.x,landmark.y),1,(255,0,0),1)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0XFF == ord('q'):
+        break 
+cv2.destroyAllWindows()
