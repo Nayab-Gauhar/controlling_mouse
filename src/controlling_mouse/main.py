322f7fde86e8f83cf7da6d2722aff3a354ac698a
Nayab-Gauhar <nayabgauhar8@gmail.com>
2026-08-20 18:27:25 +0530

    added the calibration technique

--- src/controlling_mouse/main.py
+++ src/controlling_mouse/main.py
@@ -1,7 +1,7 @@
-import pyautogui
 import time
 import cv2
 import dlib
+import pyautogui
 
 vid = cv2.VideoCapture(0)
 find_faces = dlib.get_frontal_face_detector()
@@ -43,6 +43,10 @@ def find_iris_location(iris_image):
         #no iris found
         return None
     return x,y
+
+top_left_average_offset = None
+bottom_right_average_offset = None
+last_time_told = None
 while True:
     ret,frame = vid.read()
     left_iris = None
@@ -69,10 +73,37 @@ while True:
         #viewing the image boxes
         # cv2.imshow('left_eye',left_iris)
         # cv2.imshow('right_eye',right_iris)
+        left_eye_center = ((landmarks[36].x + landmarks[39].x) //2 - left_bbox[0]),((landmarks[36].y+landmarks[39].y)//2 - left_bbox[2])
+        right_eye_center = ((landmarks[42].x + landmarks[45].x) //2 - right_bbox[0]),((landmarks[42].y+landmarks[45].y)//2 - right_bbox[2])
+        print(f"left-eye-centre = {left_eye_center}  right-eye-centre ={right_eye_center}")
+        left_iris_offset = None
+        right_iris_offset = None
+
         if left_iris_location is not None:
+            left_iris_offset = (left_iris_location[0]-left_eye_center[0],left_iris_location[1]-left_eye_center[1])
             cv2.circle(left_eye_frame,left_iris_location,2,(0,0,255),-1)
         if right_iris_location is not None:
+            right_iris_offset = (right_iris_location[0]-right_eye_center[0],right_iris_location[1]-right_eye_center[1])
             cv2.circle(right_eye_frame,right_iris_location,2,(0,0,255),-1)
+
+        if left_iris_location is not None and right_iris_location is not None:
+            average_offset = ((left_iris_offset[0]+right_iris_offset[0])//2 + (left_iris_offset[1]+right_iris_offset[1])//2)
+
+            needs_callibaration = (top_left_average_offset is None) or (bottom_right_average_offset is None)
+            if needs_callibaration:
+                if last_time_told is None:
+                    if top_left_average_offset is None:
+                        print("Look at top left corner")
+                    else:
+                        print("Look at bottom right corner")
+                    last_time_told = time.time()
+                elif time.time() >= last_time_told + 5:
+                    if top_left_average_offset is None:
+                        top_left_average_offset = average_offset
+                    elif bottom_right_average_offset is None:
+                        bottom_right_average_offset = average_offset
+
+                    last_time_told = None
 
     cv2.imshow('frame',frame)
     # cv2.imshow('fjls',left_eye_frame)
