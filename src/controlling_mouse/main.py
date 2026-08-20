d2d99a2a3a96526789eaaa84aa7d2874863fa092
Nayab-Gauhar <nayabgauhar8@gmail.com>
2026-08-20 13:02:50 +0530

    Refactor bounding box calculation for facial landmarks in main.py

--- src/controlling_mouse/main.py
+++ src/controlling_mouse/main.py
@@ -7,6 +7,12 @@ vid = cv2.VideoCapture(0)
 find_faces = dlib.get_frontal_face_detector()
 find_landmark = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 
+def get_bounding_box(landmarks):
+    min_x = min(landmarks , key=lambda p : p.x).x
+    max_x = max(landmarks , key = lambda p : p.x).x
+    min_y = min(landmarks , key = lambda p : p.y).y
+    max_y = max(landmarks , key = lambda p : p.y).y
+    return min_x,max_x,min_y,max_y
 
 while True:
     ret,frame = vid.read()
@@ -17,6 +23,13 @@ while True:
         landmarks = find_landmark(frame,faces).parts()
         for landmark in landmarks:
             cv2.circle(frame,(landmark.x,landmark.y),1,(255,0,0),1)
+
+        left_bbox = get_bounding_box(landmarks[36:42])
+        right_bbox = get_bounding_box(landmarks[42:48])
+
+        cv2.rectangle(frame,(left_bbox[0],left_bbox[2]),(left_bbox[1],left_bbox[3]),(0,255,0),1)
+        cv2.rectangle(frame,(right_bbox[0],right_bbox[2]),(right_bbox[1],right_bbox[3]),(0,255,0),1)
+
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0XFF == ord('q'):
         break 
